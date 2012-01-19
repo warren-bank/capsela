cc87bf2f7a364b7e6a0ffa6757012b501f805409
Seth Purcell <seth.purcell@sitelier.com>
2012-01-19 16:12:35 -0500

    added new App class to handle setup and shutdown of services, as well as global concerns such as a global exception handling, privilege revocation via setuid, etc.

---
+++ testing/tests/AppTest.js
@@ -0,0 +1,64 @@
+/*!
+ * Copyright (C) 2011 Sitelier Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Author: Seth Purcell
+ * Date: 1/19/12
+ */
+
+"use strict";
+
+var capsela = require('capsela');
+var App = capsela.App;
+var mp = require('capsela-util').MonkeyPatcher;
+var Log = require('capsela-util').Log;
+
+exports.basics = {
+
+    setUp: mp.setUp,
+    tearDown: mp.tearDown,
+    
+    "test init": function(test) {
+
+        test.expect(3);
+
+        var gecb;
+        
+        mp.patch(process, 'on', function(event, cb) {
+            test.equal(event, 'uncaughtException');
+            gecb = cb;
+        });
+
+        var app = new App();
+        var err = new Error("Oh God, no!");
+
+        // patch the log
+        app.log.log = function(priority, message) {
+            test.equal(priority, Log.ERROR);
+            test.equal(message, 'UNCAUGHT EXCEPTION: ' + err + '\n' + err.stack);
+        };
+
+        // pretend a global exception was thrown
+        gecb(err);
+
+        test.done();
+    }
+}
